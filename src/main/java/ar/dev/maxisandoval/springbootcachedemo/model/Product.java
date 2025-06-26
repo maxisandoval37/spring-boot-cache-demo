e075d7136e220eca80679b765aa76d594e04b5a6
Maximiliano Sandoval <maxisandoval98@gmail.com>
2025-06-26 18:14:58 -0300

    Fix JSON deserialization and tidy POM

--- src/main/java/ar/dev/maxisandoval/springbootcachedemo/model/Product.java
+++ src/main/java/ar/dev/maxisandoval/springbootcachedemo/model/Product.java
@@ -1,10 +1,12 @@
 package ar.dev.maxisandoval.springbootcachedemo.model;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Product {
     private Long id;
     private String name;
